bf8bb2b6bab33be8bee8807845587aacf70b3f37
VazhaK14 <khayrivazhasocial@gmail.com>
2025-10-29 01:28:59 +0700

    fix: remove npm and yarn

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '../../generated/prisma';
 
 const prisma = new PrismaClient();
 
-export const auth = betterAuth({
+export const auth: ReturnType<typeof betterAuth> = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
