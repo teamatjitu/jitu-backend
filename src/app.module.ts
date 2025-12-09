942e8997c976779b12b3fd0ca4ddab284610fe35
Hakim Nizami <hakimnizami15@gmail.com>
2025-12-09 15:08:52 +0700

    feat: finalized auth config for dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { auth } from './lib/auth';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule.forRoot({ auth })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    AuthModule.forRoot({ auth }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,25 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL as string,
+});
 
 const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL as string,
+  adapter,
 });
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    disableCSRFCheck: true,
+    disableOriginCheck: true,
+  },
 });
