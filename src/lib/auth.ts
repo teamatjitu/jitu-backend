e5f9ba9ade42d5dff78310091acdf944e114bb29
VazhaK14 <khayrivazhasocial@gmail.com>
2026-01-05 18:28:33 +0700

    fix: origin typo

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,10 +14,7 @@ const prisma = new PrismaClient({
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
-  trustedOrigins: [
-    'http://localhost:5173',
-    'https://https://jitu-frontend.vercel.app',
-  ],
+  trustedOrigins: ['http://localhost:5173', 'https://jitu-frontend.vercel.app'],
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
