894f5e267664e25ff352d0e12f7deebd115439e8
VazhaK14 <khayrivazhasocial@gmail.com>
2026-01-05 15:37:55 +0700

    fix: deployment 2

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
     seed: 'tsx prisma/seed.ts',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL ?? env('DATABASE_URL'),
   },
 });
