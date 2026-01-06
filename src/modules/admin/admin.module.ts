6b32b237b3e565a932c4cd12a6fb5bde52b2e27c
VazhaK14 <khayrivazhasocial@gmail.com>
2026-01-06 14:59:58 +0700

    feat: ccrud subtest

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './services/admin.service';
 import { AdminController } from './admin.controller';
+import { AdminTryoutService } from './services/tryout.service';
+import { AdminSubtestService } from './services/subtest.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, AdminTryoutService, AdminSubtestService],
 })
 export class AdminModule {}
