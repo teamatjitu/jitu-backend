ea120792dd8cef043873a714c615c185093c421c
VazhaK14 <khayrivazhasocial@gmail.com>
2026-01-08 21:48:20 +0700

    fix: schema

--- prisma/migrations/20251228170856_useranswer_question/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "UserAnswer" ADD CONSTRAINT "UserAnswer_question_id_fkey" FOREIGN KEY ("question_id") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20260105152428_update_tryout_admin/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Subtest" DROP CONSTRAINT "Subtest_try_out_id_fkey";
-
--- AlterTable
-ALTER TABLE "TryOut" ADD COLUMN     "status" "TryoutStatus" NOT NULL DEFAULT 'NOT_STARTED';
-
--- AddForeignKey
-ALTER TABLE "Subtest" ADD CONSTRAINT "Subtest_try_out_id_fkey" FOREIGN KEY ("try_out_id") REFERENCES "TryOut"("id") ON DELETE CASCADE ON UPDATE CASCADE;
