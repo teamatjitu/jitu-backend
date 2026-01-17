3e40709533ba25ba3eeb43185bc1cb59573a2a36
VazhaK14 <khayrivazhasocial@gmail.com>
2026-01-17 07:18:02 +0700

    chore: add default cookies attribute

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,13 +4,6 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-console.log('--- Auth Config Debug ---');
-console.log('BETTER_AUTH_URL:', process.env.BETTER_AUTH_URL);
-console.log('DATABASE_URL exists:', !!process.env.DATABASE_URL);
-console.log('BETTER_AUTH_SECRET exists:', !!process.env.BETTER_AUTH_SECRET);
-console.log('GOOGLE_CLIENT_ID exists:', !!process.env.GOOGLE_CLIENT_ID);
-console.log('-------------------------');
-
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL as string,
 });
@@ -50,8 +43,8 @@ export const auth = betterAuth({
   advanced: {
     disableOriginCheck: true,
     disableCSRFCheck: true,
-    // defaultCookieAttributes: {
-    //   sameSite: 'None',
-    // },
+    defaultCookieAttributes: {
+      sameSite: 'None',
+    },
   },
 });
